0eed4c357f30b7ce4b63501e99368e4ce205fbde
Jared Lutz and semhar kidane <jared@>
2018-04-19 14:39:16 -0700

    add images to webpack and src folder

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,12 @@ module.exports = {
         ]
       },
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
